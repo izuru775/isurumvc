3e9efb58ed186b513ee94e5b8a511f74e704704f
izuru775 <izuru775@gmail.com>
2022-07-14 21:29:36 +1000

    isuruMVC PHP template created

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+    // DB Params
+    define('DB_HOST','localhost');
+    define('DB_USER','Isuru');
+    define('DB_PASS','123456');
+    define('DB_NAME','imvc');
+
+    // App Roo
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL ROOT
+    define('URLROOT','http://localhost/isurumvc');
+    // Site Name
+    define('SITENAME','IsuruMVC');
+    
